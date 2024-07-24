42b4ca3e0e343587cbe567b3d6f198b1e3a0d4f5
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-07-24 14:34:56 +0300

    Open file picker to pick file

--- src/ui/styles/components/grid-toolbar/grid-toolbar.tsx
+++ src/ui/styles/components/grid-toolbar/grid-toolbar.tsx
@@ -10,9 +10,18 @@ type GridToolbarProps = {
 
 const removeAction = { key: 'remove-selected', name: 'Remove selected', className: 'danger' };
 
+const onFilePickerOpen = (callback: (file?: File) => void) => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = (e) => callback((e.target as HTMLInputElement)?.files?.[0]);
+    input.click();
+};
+
 export const GridToolbar = ({ isRemoveDisabled, hasChanges, onSave, onSavePublish, onRemove }: GridToolbarProps) => {
+    const sendFileToServer = () => {};
+
     const actions = [
-        { key: 'import', name: 'Import', onSelect: () => {} },
+        { key: 'import', name: 'Import', onSelect: () => onFilePickerOpen(sendFileToServer) },
         { key: 'export', name: 'Export', disabled: hasChanges, onSelect: () => {} },
         { ...removeAction, disabled: isRemoveDisabled, onSelect: onRemove },
     ];
