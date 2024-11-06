eae9c6e0110a6085a3e87a00457bdb03c369ea4d
Morel Sébastien <morel.seb@gmail.com>
2024-11-06 09:56:45 -0800

    fix: don't load too much folders

--- src/infrastructure/crystallize/fetch-folders.server.ts
+++ src/infrastructure/crystallize/fetch-folders.server.ts
@@ -12,6 +12,16 @@ type Node = {
 };
 export const fetchFolders = async (language: string, { apiClient }: Deps): Promise<Node[]> => {
     const fetcher = createNavigationFetcher(apiClient);
-    const response = await fetcher.byFolders('/', language, 10);
-    return response.tree.children;
+    const response = await fetcher.byFolders('/', language, 3);
+    return response.tree.children.flatMap((folder: { children?: any[] }) => {
+        if (!folder.children) {
+            return [];
+        }
+        return [...folder.children, ...folder.children.flatMap((subfolder: { children?: any[] }) => {
+            if (!subfolder.children) {
+                return [];
+            }
+            return subfolder.children
+        })];
+    });
 };
